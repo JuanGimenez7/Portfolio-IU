e8984d35bbfb5f76b0f18e61f953c7282b5a02fb
JuanGimenez7 <142836425+JuanGimenez7@users.noreply.github.com>
2025-02-04 04:44:14 -0400

    Incorporacion exitosa de la plantilla

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 import { routes } from './app.routes';
 
